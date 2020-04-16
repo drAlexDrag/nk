0382ad9ffe720382acb87a1bb12f426d56d5a42d
Dron <dralexdrag@gmail.com>
2020-04-16 08:48:43 +0300

    izm

--- catalog/catalog_search.php
+++ catalog/catalog_search.php
@@ -1,8 +1,11 @@
 <?php
 require_once '../connect.php';
 if (isset($_POST)){
-	$beans=R::getAll('SELECT ncatalog.id, ncatalog.ncatalog_name, ncatalog.ncatalog_number, ncatalog.ncatalog_cabinet, ncatalog.authority
+	$beans=R::getAll('SELECT ncatalog.id, ncatalog.ncatalog_name, ncatalog.ncatalog_number, ncatalog.ncatalog_cabinet, ncatalog.authority, ncatalog.unit_id, ncatalog.department_id, ncatalog.sector_id, unit.unit_name, department.department_name, sector.sector_name
     FROM ncatalog
+    INNER JOIN unit ON ncatalog.unit_id = unit.id
+    INNER JOIN department ON ncatalog.department_id = department.id
+    INNER JOIN sector ON ncatalog.sector_id = sector.id
  WHERE  ncatalog_number LIKE "%'.$_POST["searchString"].'%" 
  OR ncatalog_name LIKE "%'.$_POST["searchString"].'%" 
  OR ncatalog_cabinet LIKE "%'.$_POST["searchString"].'%"
@@ -15,6 +18,8 @@ if (isset($_POST)){
   <tr>
   <th>Абонент</th>
   <th>Телефон</th>
+  <th>Управление</th>
+  <th>Отдел/Бюро</th>
   <th>Кабинет</th>
   </tr>';
   foreach($beans as $row)
@@ -23,6 +28,8 @@ if (isset($_POST)){
    <tr>
    <td class="info">'.$row["ncatalog_name"].'</td>
    <td>'.$row["ncatalog_number"].'</td>
+   <td>'.$row["unit_name"].'</td>
+   <td>'.$row["department_name"].'</td>
    <td>'.$row["ncatalog_cabinet"].'</td>
    </tr>
    ';
